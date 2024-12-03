04bd5f81641dd67d83de300a33587da99718eb2b
Abhisar Garg <garg.abhisar@gmail.com>
2024-12-03 13:12:34 +0530

    Login Component - Validators Added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginForm = new FormGroup({
+    useremail: new FormControl('', [Validators.required, Validators.email]),
+    userpassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  });
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private webapi: ApiService) { 
+    
+  }
+
+  onSubmit() {
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    alert(
+      this.loginForm.value.useremail + ' | ' + this.loginForm.value.userpassword
+    );
+
+    //let loginresult = this.webapi.login(this.loginForm.value);
+
+    // loginresult.subscribe((data: any) => {
+    //   if(data.statusCode == 400)
+    //   {
+    //     Swal.fire({
+    //       title: data.validation[0].title,
+    //       text: data.validation[0].details,
+    //       icon: 'error',
+    //       confirmButtonText: 'Ok'
+    //     });
+    //   }
+    //   else{
+    //     sessionStorage.setItem('result', JSON.stringify(data.result));
+    //     this.router.navigateByUrl('/home/dashboard')
+    //   }
+    // })
+  }
 }
