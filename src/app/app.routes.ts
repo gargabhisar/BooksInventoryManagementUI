fd4b911055daf3e77ad2bdf818dfe220be4b6e88
Abhisar Garg <garg.abhisar@gmail.com>
2024-12-07 14:18:09 +0530

    Removed Search Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { BookSearchComponent } from './book-search/book-search.component';
 import { BooksComponent } from './books/books.component';
 
 export const routes: Routes = [
@@ -12,7 +11,6 @@ export const routes: Routes = [
         path: '', component: HomeComponent,
         children: [
             { path: 'dashboard', component: DashboardComponent },
-            { path: 'booksearch', component: BookSearchComponent },
             { path: 'books', component: BooksComponent }
         ]
     }
