75e4eb062d6edd6f8ec192b9d21da993ff2d9c8c
Abhisar Garg <garg.abhisar@gmail.com>
2024-12-09 22:36:30 +0530

    Added Alphabets Buttons

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
 export class BooksComponent {
+  alphabets: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  selectedLetter: string = 'A'; // Default selected letter is 'A'
 
+  selectLetter(letter: string): void {
+    this.selectedLetter = letter;
+  }
 }
