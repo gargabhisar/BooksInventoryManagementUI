2c369cc9bcd4d7aa6d9b3f01b42f225323223872
Abhisar Garg <garg.abhisar@gmail.com>
2024-12-12 20:48:14 +0530

    Updated Interceptor to redirect to login on Token Expiration

--- src/app/book-update/book-update.component.ts
+++ src/app/book-update/book-update.component.ts
@@ -14,6 +14,7 @@ import Swal from 'sweetalert2';
 })
 export class BookUpdateComponent {
   book!: Book;
+  isDisabled = false;
 
   constructor(private router: Router, private webapi: ApiService) { }
 
@@ -34,7 +35,7 @@ export class BookUpdateComponent {
         });
       }
       else {
-        this.book = book;
+        this.isDisabled = true;
       }
     })
   }
--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,11 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
-  // Inject the APIService
+  // Inject the Router, API Servicec
+  const router = inject(Router);
   const apiService = inject(ApiService);
 
   // Get the token from sessionStorage
@@ -17,5 +22,32 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     },
   });
 
-  return next(clonedRequest);
+  return next(clonedRequest).pipe(
+    catchError((error) => {
+      // console.log('error is intercept')
+      // console.error(error);
+      if (error instanceof HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+          console.error("Error Event");
+        } else {
+          console.log(`error status : ${error.status} ${error.statusText}`);
+          switch (error.status) {
+            case 401:      //login
+              router.navigateByUrl("/login");
+              break;
+            case 403:     //forbidden
+              router.navigateByUrl("/unauthorized");
+              break;
+          }
+        }
+      } else {
+        console.error("some thing else happened");
+      }
+      return throwError(error.message);
+    }),
+    finalize(
+      () => {
+      }
+    )
+  );
 };
