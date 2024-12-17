1ae105627ed6bc251961182019b7b2a8e27e7a14
Abhisar Garg <garg.abhisar@gmail.com>
2024-12-17 22:11:22 +0530

    Updated Home Menu & Side Bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,40 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faHouse, faBook, faCircleUser, faPowerOff } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faBook } from '@fortawesome/free-solid-svg-icons';
 import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   faHouse = faHouse;
   faBook = faBook;
-  faCircleUser = faCircleUser;
-  faPowerOff = faPowerOff;
 
   isActive: boolean = false;
 
-  Name : string = 'Login User';
-  Role : string = 'Role';
-  
+  Name: string = 'Login User';
+  Role: string = 'Role';
+
   constructor(private webapi: ApiService) {
     var loggedInUser = webapi.getLoginDetails();
     this.Name = loggedInUser.name;
     this.Role = loggedInUser.role;
   }
 
-  clearalphabet()
-  {
+  clearalphabet() {
     this.webapi.clearalphabet();
   }
 
-  logout()
-  {
+  logout() {
     sessionStorage.clear();
   }
 
