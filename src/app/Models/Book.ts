4f3121420f22ca57a44f1dd768740964027cbfaf
Abhisar Garg <garg.abhisar@gmail.com>
2024-12-19 02:40:19 +0530

    Added Box In Book & Book Update

--- src/app/Models/Book.ts
+++ src/app/Models/Book.ts
@@ -1,15 +1,15 @@
 export interface BookLocation {
-    almirah: number;
-    bed: number;
-  }
-  
-  export interface Book {
-    bookId: string;
-    image: string; // Base64 encoded string
-    title: string;
-    author: string;
-    isbn: string;
-    locations: BookLocation;
-    lastUpdatedOn: Date;
-  }
-  
+  almirah: number;
+  bed: number;
+  box: number;
+}
+
+export interface Book {
+  bookId: string;
+  image: string; // Base64 encoded string
+  title: string;
+  author: string;
+  isbn: string;
+  locations: BookLocation;
+  lastUpdatedOn: Date;
+}
